621f2f5ef9f73b8ee4b1adea328008afa1d39528
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2026-09-18 13:18:53 +0300

    Update HelloWorldController.java

--- src/main/java/com/shipfmrsamplejava/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/shipfmrsamplejava/helloworld/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
